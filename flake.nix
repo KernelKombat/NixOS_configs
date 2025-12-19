b99c15f9697546d80a6a083e1f7dbd9f93253fb0
KernelKombat <165580580+KernelKombat@users.noreply.github.com>
2025-12-19 23:31:53 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    jovian.follows = "chaotic/jovian";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, chaotic, jovian,  ... }:
+  outputs = { self, nixpkgs, nixpkgs-stable,  ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -25,8 +23,6 @@
        };
       modules = [
         ./configuration.nix
-	jovian.nixosModules.default
-        chaotic.nixosModules.default
       ];
     };
   };
